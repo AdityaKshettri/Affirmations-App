22d814605fe739abd9e7212fac9e1991d79ae865
AdityaKshettri <adikshettri1623@gmail.com>
2022-08-19 00:51:03 +0530

    added affirmation generation logic

--- app/src/main/java/com/aditya/project/model/Affirmation.kt
+++ app/src/main/java/com/aditya/project/model/Affirmation.kt
@@ -1,4 +1,9 @@
 package com.aditya.project.model
 
-data class Affirmation(val stringResourceId: Int) {
-}
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Affirmation(
+    @StringRes val stringResourceId: Int,
+    @DrawableRes val imageResourceId: Int
+)
